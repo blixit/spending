15fcf0e0bd7f7c04273685b736e4a39c8bc222d2
alain <developer2.reliefapps@gmail.com>
2019-09-23 23:33:57 +0200

    added travis

--- src/utils/form.js
+++ src/utils/form.js
@@ -1,10 +1,11 @@
 /**
- * Removes the listed fields from the source
- * @param {*} form The source object
- * @param {*} fields The fields list
- */
+* Removes the listed fields from the source
+* @param {*} form The source object
+* @param {*} fields The fields list
+*/
 export const clean = (form, fields) => {
   const result = {};
+  // eslint-disable-next-line no-unused-vars
   for (const field in form) {
     if (!form.hasOwnProperty(field) || fields.includes(field)) {
       continue;
